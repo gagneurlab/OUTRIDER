b2a9fc9346008d5f3898ba4e108bbd64d084fdb0
brechtma <brechtma@in.tum.de>
2018-06-25 17:20:23 +0200

    shorter lines

--- R/method-plot.R
+++ R/method-plot.R
@@ -795,7 +795,8 @@ plotPowerAnalysis <- function(ods){
     dt<-rbindlist(lapply(c(0,0.1,0.2,0.3,0.5, 2,5,10), function(frac) 
         data.table(mean=m, disp=d, frac=frac, 
             pVal=pmin(0.5, pnbinom(frac * m, mu = m, size=d),
-                1 - pnbinom(frac * m, mu = m, size=d) + dnbinom(frac * m, mu = m, size=d)
+                1 - pnbinom(frac * m, mu = m, size=d) + 
+                    dnbinom(frac * m, mu = m, size=d)
             )
         )))
     
