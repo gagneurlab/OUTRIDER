d91c5f49acbb15b1cbcbb32fa8b32a3858499b86
Christian Mertes <mertes@in.tum.de>
2018-06-25 16:20:47 +0200

    cleanup results function and bugfixes

--- R/method-results.R
+++ R/method-results.R
@@ -1,6 +1,5 @@
 
-compileResults <- function(object, padjCutoff=0.05, zScoreCutoff=0, 
-                    round=TRUE, all=FALSE){
+compileResults <- function(object, padjCutoff, zScoreCutoff, round, all){
     features <- c("pValue", "padjust", "zScore", "l2fc")
     if(!is(object, 'OutriderDataSet')){
         stop('Please provide an OutriderDataSet')
@@ -19,8 +18,9 @@ compileResults <- function(object, padjCutoff=0.05, zScoreCutoff=0,
                 "\n\tods <- OUTRIDER(ods)"))
     }
     
-    if(all==FALSE){
-        object <- object[rowMins(assays(object)[['padjust']]) <= padjCutoff]
+    if(isFALSE(all)){
+        object <- object[aberrant(ods, padjCutoff=padjCutoff, 
+                zScoreCutoff=zScoreCutoff, by="gene") > 0,]
         if(dim(object)[1]==0){
             warning('No significant events: use all=TRUE to print all counts.')
             return(data.table(geneID='a', sampleID='a', pValue=0.1, padjust=0.1,
@@ -36,6 +36,8 @@ compileResults <- function(object, padjCutoff=0.05, zScoreCutoff=0,
     countdataDF <- as.data.table(counts(object), keep.rownames="geneID")
     countNormDF <- as.data.table(counts(object, normalized=TRUE), 
             keep.rownames="geneID")
+    aberrantDF <- as.data.table(keep.rownames="geneID", aberrant(
+            object, padjCutoff=padjCutoff, zScoreCutoff=zScoreCutoff))
     
     featureDTs <- lapply(features, function(f){
         dt <- as.data.table(assays(object)[[f]], keep.rownames='geneID')
@@ -43,43 +45,40 @@ compileResults <- function(object, padjCutoff=0.05, zScoreCutoff=0,
     names(featureDTs) <- features
     
     featureDTs <- append(featureDTs, list(
-            rawcounts=countdataDF, normcounts=countNormDF))
+            rawcounts=countdataDF, normcounts=countNormDF, aberrant=aberrantDF))
     
     featureMeltDTs <- lapply(names(featureDTs), function(n){
         featureDTs[[n]] %>% melt(id.vars="geneID", value.name=n,
                 measure.vars=2:(dim(object)[2]+1), variable.name="sampleID")})
     
-    tidyresults <- featureMeltDTs[[1]]
-    for(i in 2:length(featureMeltDTs)){
-        tidyresults <- merge(tidyresults, featureMeltDTs[[i]], 
-                by=c("geneID", "sampleID"))
-    }
+    tidyresults <- Reduce(x=featureMeltDTs, f=function(x, y){
+        merge(x, y, by=c("geneID", "sampleID")) })
     
     tidyresults <- merge(tidyresults, DTfitparameters, by=c('geneID'))
     
-    tidyresults <- merge(tidyresults, data.table(geneID = rownames(object), 
-            meanCorrected = rowMeans(counts(object, normalized=TRUE))), 
+    tidyresults <- merge(tidyresults, data.table(geneID=rownames(object), 
+            meanCorrected=rowMeans(counts(object, normalized=TRUE))), 
             by=c('geneID'))
     
     tidyresults <- merge(tidyresults, data.table(sampleID=colnames(object), 
-            AberrantBySample = aberrant(object, by='sample')), 
+            AberrantBySample=aberrant(object, by='sample')), 
             by=c('sampleID'))
     
-    tidyresults <- merge(tidyresults, data.table(geneID = rownames(object), 
-            AberrantByGene = aberrant(object, by='gene')), 
+    tidyresults <- merge(tidyresults, data.table(geneID=rownames(object), 
+            AberrantByGene=aberrant(object, by='gene')), 
             by=c('geneID'))
     
-    if(all==FALSE){
-        tidyresults <- tidyresults[
-                padjust <= padjCutoff & abs(zScore) >= zScoreCutoff][
-                        order(padjust)]
+    if(isFALSE(all)){
+        tidyresults <- tidyresults[aberrant == TRUE]
     }
-    tidyresults[,padj_rank := rank(padjust), by = sampleID]
+    tidyresults[,padj_rank:=rank(padjust), by=sampleID]
+    tidyresults <- tidyresults[order(padjust)]
     
-    if(round == TRUE){
-        tidyresults[,c("normcounts", "mu", "zScore", "l2fc", "disp"):=list(
-                round(normcounts, 2), round(mu, 2), round(zScore,2), 
-                round(l2fc, 2), round(disp, 2))]
+    if(isTRUE(round)){
+        col2round <- c("normcounts", "mu", "zScore", "l2fc", "disp",
+                "meanCorrected")
+        devNull <- sapply(col2round, function(x){
+                tidyresults[,c(x):=round(get(x), 2)] })
     }
     return(tidyresults)
 }
@@ -120,7 +119,8 @@ setGeneric("results", function(object, ...) standardGeneric("results"))
 #' @export results
 #' @export
 setMethod("results", "OutriderDataSet", function(object, 
-                    padjCutoff=0.05, zScoreCutoff=0, round=TRUE, all=FALSE){
-    compileResults(object, padjCutoff=0.05, zScoreCutoff=0,
-            round=TRUE, all=FALSE)
+                    padjCutoff=0.05, zScoreCutoff=0, round=TRUE, all=FALSE, 
+                    ...){
+    compileResults(object, padjCutoff=padjCutoff, zScoreCutoff=zScoreCutoff,
+            round=round, all=all, ...)
 })
