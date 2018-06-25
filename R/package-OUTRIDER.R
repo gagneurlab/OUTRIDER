27181312acd0778d431aa7b2030abe56f9c7524f
Christian Mertes <mertes@in.tum.de>
2018-06-25 18:36:45 +0200

    update imports and exports

--- R/package-OUTRIDER.R
+++ R/package-OUTRIDER.R
@@ -7,6 +7,8 @@
 #'
 #' @import data.table
 #' 
+#' @importFrom methods as is new validObject
+#' 
 #' @importFrom Biobase rowMax
 #' 
 #' @importFrom BiocGenerics estimateSizeFactors plotDispEsts
@@ -15,82 +17,66 @@
 #'          sizeFactors sizeFactors<- counts counts<-
 #'          DESeqDataSetFromMatrix DESeqDataSet
 #'          makeExampleDESeqDataSet show fpkm fpm
+#'          estimateSizeFactorsForMatrix
 #' 
-#' @importFrom SummarizedExperiment colData colData<- assays assays<- 
-#'          assayNames mcols mcols<- width rowData assay assay<-
+#' @importFrom SummarizedExperiment colData colData<- assays assays<-
+#'          assayNames mcols mcols<- assay assay<-
 #' 
-#' @importFrom GenomicFeatures makeTxDbFromGFF exonsBy
+#' @importFrom BBmisc isScalarLogical isScalarNumeric isScalarCharacter isFALSE
 #' 
-#' @importFrom GenomicRanges reduce
-#' 
-#' @importFrom BiocParallel bplapply bpparam MulticoreParam
-#' 
-#' @importFrom tidyr %>%
-#' 
-#' @importFrom stats p.adjust setNames sd dnbinom quantile optim var pnbinom 
-#'          dnbinom median ppoints qbeta runif cor cutree hclust dist lm predict
-#'          rnorm glm Gamma
-#' 
-#' @importFrom methods validObject is new as 
-#' 
-#' @importFrom ggplot2 ggplot geom_point aes geom_hline geom_vline 
-#'          geom_vline ggtitle theme element_text geom_tile geom_smooth
-#'          geom_segment scale_colour_manual scale_y_log10 labs unit
-#'          geom_histogram scale_x_log10 scale_fill_manual ylim
-#'          
-#' 
-#' @importFrom scales trans_format math_format         
-#'         
-#' @importFrom DelayedArray rowMins
-#' 
-#' @importFrom S4Vectors DataFrame rowSums colSums metadata metadata<-
-#' 
-#' @importFrom utils read.table head compareVersion
-#' 
-#' @importFrom graphics plot abline grid points mtext polygon axis text box
-#'         legend title lines par
-#' 
-#' @importFrom gplots barplot2 heatmap.2
-#' 
-#' @importFrom matrixStats rowSds rowMedians rowQuantiles
-#' 
-#' @importFrom plotly plot_ly add_markers ggplotly plotly_build style layout
-#' 
-#' @importFrom RColorBrewer brewer.pal
-#' 
-#' @importFrom BBmisc isScalarLogical isScalarNumeric isScalarInteger 
-#'         isScalarCharacter isScalarValue isFALSE
-#' 
-#' @importFrom reticulate import py_get_attr
-#' 
-#' @importFrom LSD heatscatter
-#' 
-#' @importFrom pcaMethods pca loadings
+#' @importFrom BiocParallel bplapply bpparam
 #' 
 #' @importFrom compiler cmpfun
 #' 
+#' @importFrom GenomicFeatures makeTxDbFromGFF exonsBy
+#' 
+#' @importFrom GenomicRanges reduce width
+#' 
+#' @importFrom ggplot2 aes geom_histogram geom_smooth geom_tile geom_point 
+#'          ggplot labs scale_x_log10 scale_fill_manual theme ylim
+#'          
+#' @importFrom gplots barplot2 bluered heatmap.2
+#' 
+#' @importFrom graphics plot abline axis box grid legend mtext par points 
+#'          polygon text title
+#' 
+#' @importFrom matrixStats rowSds rowMedians rowQuantiles
+#' 
+#' @importFrom pcaMethods pca loadings
+#' 
+#' @importFrom plotly %>% ggplotly layout plotly_build plot_ly
+#' 
+#' @importFrom plyr .
+#' 
+#' @importFrom RColorBrewer brewer.pal
+#' 
 #' @importFrom Rcpp sourceCpp
+#' 
+#' @importFrom reticulate import py_get_attr
+#' 
+#' @importFrom S4Vectors DataFrame metadata metadata<-
+#' 
+#' @importFrom scales math_format trans_format
+#' 
+#' @importFrom splines bs
+#' 
+#' @importFrom stats cor coefficients cutree dist dnbinom hclust p.adjust 
+#'          setNames sd  
+#'          quantile optim var pnbinom  median ppoints qbeta runif 
+#'            lm predict rnorm glm Gamma
+#' 
+#' @importFrom utils read.table head compareVersion
 #' 
 #' @useDynLib OUTRIDER
 #' 
-# ###
-# # TODO old package to be checked again!
-#' 
-#' @importFrom gplots  bluered
-#' @importFrom tidyr gather
-#' @importFrom stats as.formula complete.cases coefficients relevel
-#' @importFrom plyr join .
-#' 
-#' 
-
 NULL
 
 #' 
 #' TODO This is to get rid of the warnings of undefined variables
 #'     due to the nature of data.table and ggplot/plotly
 #' @noRd
-globalVariables(c("disp", "padjust", "pValue", "zScore", "padj_rank", "mu", 
-        "l2fc", "sampleID", "normcounts", "sample.rank", "N2", "color", "obs",
-        "Var1", "Var2", "value", "medianCts", "norm_rank", ".x",
-        "passedFilter", "loggeomeans"), package="OUTRIDER")
+globalVariables(c("color", "disp", "frac", "Fraction", "loggeomeans", 
+        "medianCts", "mu", "negLog10pVal", "normcounts", "norm_rank", "obs", 
+        "padj_rank", "padjust", "passedFilter", "pValue", "sampleID", 
+        "value", "Var1", "Var2", ".x", "zScore"), package="OUTRIDER")
 
