abfc2c05e30dc2a064fc3f2cbde7613096136214
brechtma <brechtma@in.tum.de>
2018-06-25 10:01:10 +0200

    typo in user input check

--- R/method-plot.R
+++ R/method-plot.R
@@ -31,7 +31,7 @@ plotQQ <- function(ods, geneID=NULL, global=FALSE, padjCutoff=0.05,
                 zScoreCutoff=0, main=NULL, sample=TRUE, legendPos="topleft",
                 outlierRatio=0.001, conf.alpha=0.05, pch=16, xlim=NULL, ylim=NULL,
                 col=NULL, ...){
-    if(!is(object, 'OutriderDataSet')){
+    if(!is(ods, 'OutriderDataSet')){
         stop('Please provide an OutriderDataSet')
     }
     if(is.null(geneID)&isFALSE(global)){
