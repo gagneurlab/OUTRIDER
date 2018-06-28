a94896484137679fd82b03bc245a1d552a35a90d
brechtma <brechtma@in.tum.de>
2018-06-28 10:32:59 +0200

    improved tests

--- tests/testthat/test_Rautoenc_loss.R
+++ tests/testthat/test_Rautoenc_loss.R
@@ -1,3 +1,4 @@
+context("Test loss and gradient functions: ")
 
 # w : weights
 # k : read counts matrix (sample x genes)
@@ -21,9 +22,6 @@ k <- apply(
         rnbinom(n, mu=s*exp(yi), size=theta)
 )
 
-## check that the simulated counts seem to make sense
-# hist(log10(1+k))
-
 # compute x
 x0 <- log((1+k)/s)
 xbar <- colMeans(x0)
@@ -31,12 +29,17 @@ x <- t(t(x0)-xbar)
 
 # ## value for random weight init
 w <- c(rnorm(p*q, sd=1/p*q), rep(0,p))
+
 # expect a real positiv number.
-loss(w, k, x, s, xbar, theta)
+test_that("loss is real number", {
+    l <- loss(w, k, x, s, xbar, theta)
+    expect_that(l ,is.numeric)
+    expect_that(length(l) == 1)
+})
 
 # numeric Gradient
 numericLossGrad <- function(fn, epsilon, w,...){
-    grad <- list()
+    grad <- numeric(length(w))
     for(i in seq_along(w)){
         eps <- integer(length(w))
         eps[i] <- epsilon
@@ -49,8 +52,12 @@ numericLossGrad <- function(fn, epsilon, w,...){
 #plot(numericLossGrad(loss, 1E-8, w, k=k, x=x, s=s, xbar=xbar, theta=25),
 #     lossGrad(w, k, x, s, xbar, theta));abline(0,1)
 
-for(i in 1:5){
-    w <- c(rnorm(p*q, sd=1/p*q), rep(0,p))
-    expect_equal(lossGrad(w, k, x, s, xbar, theta), 
-        matrix(as.numeric(numericLossGrad(loss, 1E-8, w, k=k, x=x, s=s, xbar=xbar, theta=25)), ncol=q+1), tol=0.0001)
-}
+test_that("Analytic gradient ~= numeric gradient", {
+    for(i in 1:5){
+        w <- c(rnorm(p*q, sd=1/p*q), rep(0,p))
+        expect_equal(lossGrad(w, k, x, s, xbar, theta), 
+            matrix(numericLossGrad(
+                    loss, 1E-8, w, k=k, x=x, s=s, xbar=xbar, theta=theta
+                        ), ncol=q+1), tol=0.0001)
+    }
+})
