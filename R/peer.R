6773195956fcef8b97fbe7bb9acbc741aebc6c6f
Christian Mertes <mertes@in.tum.de>
2018-08-20 08:40:02 +0200

    cleanup of peer code

--- R/peer.R
+++ R/peer.R
@@ -1,6 +1,10 @@
 
-
+#'
+#' PEER implementation
+#'
+#' @noRd
 peer <- function(ods, maxItr=1000){
+    
     # check for PEER
     if(!require(peer)){
         stop("Please install the 'peer' package from GitHub to use this ",
@@ -38,57 +42,3 @@ peer <- function(ods, maxItr=1000){
     return(ods)
 }
 
-runSva <- function(ods){
-    ods <- readRDS("./../scared-analysis/Output/data/GTEx_not_sun_exposed_OutriderDONE.RDS")
-    ods <- makeExampleOutriderDataSet(dataset="Kremer")[1:140, 1:140]
-    k <- counts(ods)
-    table(keep)
-    filtk <- k[keep,]
-    genes = rownames(filtk)[grep("^ENS", rownames(filtered))]
-    controls <- sample(c(TRUE, FALSE), ncol(k), replace = TRUE)
-    table(controls)
-    
-    library(zebrafishRNASeq)
-    data(zfGenes)
-    table(grepl("^ERCC", rownames(zfGenes)))
-    table(grepl("^ENS", rownames(zfGenes)))
-    
-    controls = grepl("^ERCC", rownames(filtered))
-    
-    
-    k <- counts(ods)
-    
-    ####
-    # prepare data
-    ###
-    dim(k)
-    
-    # random group assignment
-    group = sample(c(TRUE, FALSE), ncol(k), replace = TRUE)
-    sum(group)/length(group)
-    
-    # create model
-    dat0 = as.matrix(log10(k+1))
-    mod1 = model.matrix(~group)
-    mod0 = cbind(mod1[,1])
-    
-    # estimate q
-    n.sv <- num.sv(dat0, mod0)
-    n.sv
-    
-    # run sva
-    svseq = svaseq(dat0,mod1,mod0,n.sv=15)
-    
-    svseq
-    plot(svseq,pch=19,col="blue")
-    
-    library(bladderbatch)
-    data(bladderdata)
-    pheno = pData(bladderEset)
-    
-    contrast.matrix <- cbind("C1"=c(-1,1,0,rep(0,svobj$n.sv)),"C2"=c(0,-1,1,rep(0,svobj$n.sv)),"C3"=c(-1,0,1,rep(0,svobj$n.sv)))
-    fitContrasts = contrasts.fit(fit,contrast.matrix)
-    
-
-    return(ods)
-}
