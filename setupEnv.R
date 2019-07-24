fb93d09caf29775514a5c32d43dbeae26484a5fd
Christian Mertes <mertes@in.tum.de>
2019-07-25 00:02:12 +0200

    remove isFALSE

--- setupEnv.R
+++ setupEnv.R
@@ -1,19 +1,19 @@
 # install Bioconductor dependent on the R version
 if(0 < compareVersion("3.5.0", paste(R.Version()[c("major", "minor")], collapse="."))){
-    if(isFALSE(requireNamespace("BiocInstaller"))){
+    if(!requireNamespace("BiocInstaller")){
         source("https://bioconductor.org/biocLite.R")
         biocLite(c("BiocInstaller"), Ncpus=6)
     }
     INSTALL <- BiocInstaller::biocLite
 } else {
-    if(isFALSE(requireNamespace("BiocInstaller")))
+    if(!requireNamespace("BiocInstaller"))
         install.packages("BiocManager", Ncpus=6)
     INSTALL <- BiocManager::install
 }
 
 # install needed packages
 for(p in c("devtools", "covr", "roxygen2", "BiocCheck", "R.utils"))
-    if(isFALSE(requireNamespace(p, quietly=TRUE)))
+    if(!requireNamespace(p, quietly=TRUE))
         INSTALL(p, Ncpus=6)
 
 # install OUTRIDER with its dependencies with a timeout due to travis 50 min
