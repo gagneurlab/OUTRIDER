1b8360402d0c11410cfd689cff93ebfe29f169ea
Christian Mertes <mertes@in.tum.de>
2019-07-25 06:41:21 +0200

    add messages

--- setupEnv.R
+++ setupEnv.R
@@ -1,24 +1,35 @@
 # install Bioconductor dependent on the R version
-if(0 < compareVersion("3.5.0", paste(R.Version()[c("major", "minor")], collapse="."))){
+R_VERSION <- paste(R.Version()[c("major", "minor")], collapse="."))
+message(paste0(date(), ": Current R version: ", R_VERSION))
+if(0 < compareVersion("3.5.0", R_VERSION)){
     if(!requireNamespace("BiocInstaller")){
+        message("Install BiocInstaller")
         source("https://bioconductor.org/biocLite.R")
         biocLite(c("BiocInstaller"), Ncpus=6)
     }
     INSTALL <- BiocInstaller::biocLite
 } else {
-    if(!requireNamespace("BiocInstaller"))
+    if(!requireNamespace("BiocInstaller")){
+        message("Install BiocManager")
         install.packages("BiocManager", Ncpus=6)
+    }
     INSTALL <- BiocManager::install
 }
 
 # install needed packages
-for(p in c("devtools", "covr", "roxygen2", "BiocCheck", "R.utils"))
-    if(!requireNamespace(p, quietly=TRUE))
+for(p in c("devtools", "covr", "roxygen2", "BiocCheck", "R.utils")){
+    if(!requireNamespace(p, quietly=TRUE)){
+        message(paste("Install", p))
         INSTALL(p, Ncpus=6)
+    }
+}
 
 # install OUTRIDER with its dependencies with a timeout due to travis 50 min
 R.utils::withTimeout(timeout=2400, {
+    message("Update packages")
     INSTALL(ask=FALSE, Ncpus=6)
+    
+    message("Install OUTRIDER")
     devtools::install(".", ask=FALSE, dependencies=TRUE, upgrade=TRUE)
 })
 
