98ae1b45ba2947adb6da17ffe39d4671834770a9
Christian Mertes <mertes@in.tum.de>
2019-08-27 00:57:40 +0200

    cleanup

--- setupEnv.R
+++ setupEnv.R
@@ -5,6 +5,15 @@ print_log <- function(...){
     message(paste0("\n", hash_line, "\n### ", date(), ": ", ..., "\n", hash_line, "\n"))
 }
 
+installIfReq <- function(p, type=BTYPE, Ncpus=1, ...){
+    for(j in p){
+        if(!requireNamespace(j, quietly=TRUE)){
+            print_log("Install ", j)
+            INSTALL(j, type=type, Ncpus=Ncpus, ...)
+        }
+    }
+}
+
 # install Bioconductor dependent on the R version
 R_VERSION <- paste(R.Version()[c("major", "minor")], collapse=".")
 print_log("Current R version: ", R_VERSION)
@@ -20,31 +29,24 @@ if(0 < compareVersion("3.5.0", R_VERSION)){
         print_log("Install BiocManager")
         install.packages("BiocManager", Ncpus=6)
     }
-    
     INSTALL <- BiocManager::install
 }
 
 # because of https://github.com/r-windows/rtools-installer/issues/3
 if("windows" == .Platform$OS.type){
     print_log("Install XML on windows ...")
-    INSTALL(c("XML", "xml2"), type="win.binary")
+    BTYPE <- "win.binary"
+    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", "AnnotationDbi", "BiocCheck"))
     
     print_log("Install source packages only for windows ...")
-    INSTALL(c("RSQLite", "progress"), type="win.binary")
-    INSTALL(c("AnnotationDbi", "BiocCheck"), type="win.binary")
     INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
-    
-    BTYPE <- "win.binary"
 }
 
 # install needed packages
 # add testthat to pre installation dependencies due to: https://github.com/r-lib/pkgload/issues/89
 for(p in c("XML", "xml2", "testthat", "devtools", "covr", "roxygen2", "BiocCheck", "R.utils", 
             "GenomeInfoDbData", "rtracklayer", "hms")){
-    if(!requireNamespace(p, quietly=TRUE)){
-        print_log("Install ", p)
-        INSTALL(p, type=BTYPE, Ncpus=6)
-    }
+    installIfReq(p=p, type=BTYPE, Ncpus=6)
 }
 
 # install OUTRIDER with its dependencies with a timeout due to travis 50 min
