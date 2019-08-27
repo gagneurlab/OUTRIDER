4f61db9f728e9cb8b7aca6a21b6178f371150a4a
Christian Mertes <mertes@in.tum.de>
2019-08-27 09:52:09 +0200

    make sure BiocCheck is installed

--- setupEnv.R
+++ setupEnv.R
@@ -1,11 +1,12 @@
 BTYPE <- "both"
+NCPUS <- 6
 
 print_log <- function(...){
     hash_line <- paste0(rep("#", 10), collapse="")
     message(paste0("\n", hash_line, "\n### ", date(), ": ", ..., "\n", hash_line, "\n"))
 }
 
-installIfReq <- function(p, type=BTYPE, Ncpus=1, ...){
+installIfReq <- function(p, type=BTYPE, Ncpus=NCPUS, ...){
     for(j in p){
         if(!requireNamespace(j, quietly=TRUE)){
             print_log("Install ", j)
@@ -21,13 +22,13 @@ if(0 < compareVersion("3.5.0", R_VERSION)){
     if(!requireNamespace("BiocInstaller", quietly=TRUE)){
         print_log("Install BiocInstaller")
         source("https://bioconductor.org/biocLite.R")
-        biocLite(c("BiocInstaller"), Ncpus=6)
+        biocLite(c("BiocInstaller"), Ncpus=NCPUS)
     }
     INSTALL <- BiocInstaller::biocLite
 } else {
     if(!requireNamespace("BiocManager", quietly=TRUE)){
         print_log("Install BiocManager")
-        install.packages("BiocManager", Ncpus=6)
+        install.packages("BiocManager", Ncpus=NCPUS)
     }
     INSTALL <- BiocManager::install
 }
@@ -42,19 +43,22 @@ if("windows" == .Platform$OS.type){
     INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
 }
 
+# Make sure BiocCheck is installed
+INSTALL("BiocCheck", type=BTYPE, Ncpus=NCPUS)
+
 # install needed packages
 # add testthat to pre installation dependencies due to: https://github.com/r-lib/pkgload/issues/89
 for(p in c("XML", "xml2", "testthat", "devtools", "covr", "roxygen2", "BiocCheck", "R.utils", 
             "GenomeInfoDbData", "rtracklayer", "hms")){
-    installIfReq(p=p, type=BTYPE, Ncpus=6)
+    installIfReq(p=p, type=BTYPE, Ncpus=NCPUS)
 }
 
 # install OUTRIDER with its dependencies with a timeout due to travis 50 min
 R.utils::withTimeout(timeout=2400, {
     try({
         print_log("Update packages")
-        INSTALL(ask=FALSE, type=BTYPE, Ncpus=6)
+        INSTALL(ask=FALSE, type=BTYPE, Ncpus=NCPUS)
     
         print_log("Install OUTRIDER")
-        devtools::install(".", ask=FALSE, dependencies=TRUE, upgrade=TRUE, type=BTYPE, Ncpus=6)
+        devtools::install(".", ask=FALSE, dependencies=TRUE, upgrade=TRUE, type=BTYPE, Ncpus=NCPUS)
 })})
