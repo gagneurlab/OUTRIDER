9f42d4fa3425d38c33a57fd0eeed1911ffad320c
Christian Mertes <mertes@in.tum.de>
2019-09-10 12:27:53 +0200

    Merge CI fixes and code cleanup (#16)
    * update travis
    * update travis with r install
    * update travis with no vignette build
    * include BiocCheck and remove vignette build
    * make cache
    * initial commit
    * add codecov to travis
    * update readme
    * add license
    * add version
    * add real checks
    * add tk dependency
    * check biocInstall
    * change README and License
    * do not run save/load part of vignette
    * remove python venv from cache
    * add some default packages and use them instead of installing them through pip
    * code upload
    * Add latex dependencies
    * code upload
    * make cache
    * add epochs option to autoCorrect
    * performance improvment and little bugfixes
    * new improvments and little bugfixes
    * typo in tests
    * update doc and unit tests
    * vignette upload and some little bugfixes
    * version bump
    * adding oldrel to test environment
    * remove cache
    * .travis.yml use python3.7 as default for running autocorrect
    * .travis.yml use python3.6.5 as default for running autocorrect
    
    * update typos and ignore list
    
    * add seed and make version check
    
    * change to BY
    
    * typo correction
    
    * bugfixes and better axis labels. also more paramter options
    
    * return empty data.table instead of null
    
    * updating dependencies
    
    * we used BH and not BY in the old version
    
    * sync with gitlab
    
    * update travis ci
    
    * debug travis
    
    * version bump 0.99.6wq
    
    * work on travis ci
    
    * update testing to the new annotation
    
    * remove r-devel because of biocView error
    
    * init travis with new autoencoder r version
    
    * remove devel because of BiocView error
    
    * sync with gitlab
    
    * code cleanup and adding dispest from deseq2
    
    * adjusting parameters
    
    * using OUTRIDER function instead of DESeq2
    
    * correcting tests
    
    * adding glm and Gamma functions from stats
    
    * corrected x axis in dispEsts plot
    
    * completed documentation
    
    * updated readme
    
    * updated vignette
    
    * last changes
    
    * removed SweaveOPS
    
    * add appveyor
    
    * use 5.1 for tests
    
    * typo
    
    * typo
    
    * add check force suggestes to false
    
    * use 3.4 with windows
    
    * add TxDb to install explicit
    
    * add more tests
    
    * use R for date
    
    * use devtools
    
    * make simple install
    
    * use R instead of Rscript
    
    * some changes
    
    * add real tests
    
    * add roxygen2
    
    * correct biocCheck
    
    * move to R CMD instead of R session
    
    * no build vignettes
    
    * run tests
    
    * up
    
    * no manuals

--- setupEnv.R
+++ setupEnv.R
@@ -1,6 +1,18 @@
+BTYPE <- "both"
+NCPUS <- 6
+
 print_log <- function(...){
     hash_line <- paste0(rep("#", 10), collapse="")
     message(paste0("\n", hash_line, "\n### ", date(), ": ", ..., "\n", hash_line, "\n"))
+}
+
+installIfReq <- function(p, type=BTYPE, Ncpus=NCPUS, ...){
+    for(j in p){
+        if(!requireNamespace(j, quietly=TRUE)){
+            print_log("Install ", j)
+            INSTALL(j, type=type, Ncpus=Ncpus, ...)
+        }
+    }
 }
 
 # install Bioconductor dependent on the R version
@@ -10,33 +22,42 @@ if(0 < compareVersion("3.5.0", R_VERSION)){
     if(!requireNamespace("BiocInstaller", quietly=TRUE)){
         print_log("Install BiocInstaller")
         source("https://bioconductor.org/biocLite.R")
-        biocLite(c("BiocInstaller"), Ncpus=6)
+        biocLite(c("BiocInstaller"), Ncpus=NCPUS)
     }
     INSTALL <- BiocInstaller::biocLite
 } else {
     if(!requireNamespace("BiocManager", quietly=TRUE)){
         print_log("Install BiocManager")
-        install.packages("BiocManager", Ncpus=6)
+        install.packages("BiocManager", Ncpus=NCPUS)
     }
-    
     INSTALL <- BiocManager::install
+}
+
+# because of https://github.com/r-windows/rtools-installer/issues/3
+if("windows" == .Platform$OS.type){
+    print_log("Install XML on windows ...")
+    BTYPE <- "win.binary"
+    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", "AnnotationDbi", "BiocCheck"))
+    
+    print_log("Install source packages only for windows ...")
+    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
+} else {
+    BTYPE <- "source"
 }
 
 # install needed packages
 # add testthat to pre installation dependencies due to: https://github.com/r-lib/pkgload/issues/89
-for(p in c("testthat", "devtools", "covr", "roxygen2", "BiocCheck", "R.utils")){
-    if(!requireNamespace(p, quietly=TRUE)){
-        print_log("Install ", p)
-        INSTALL(p, Ncpus=6)
-    }
+for(p in c("XML", "xml2", "testthat", "devtools", "covr", "roxygen2", "BiocCheck", "R.utils", 
+            "GenomeInfoDbData", "rtracklayer", "hms")){
+    installIfReq(p=p, type=BTYPE, Ncpus=NCPUS)
 }
 
 # install OUTRIDER with its dependencies with a timeout due to travis 50 min
 R.utils::withTimeout(timeout=2400, {
     try({
         print_log("Update packages")
-        INSTALL(ask=FALSE, Ncpus=6)
+        INSTALL(ask=FALSE, type=BTYPE, Ncpus=NCPUS)
     
         print_log("Install OUTRIDER")
-        devtools::install(".", ask=FALSE, dependencies=TRUE, upgrade=TRUE, Ncpus=6)
+        devtools::install(".", ask=FALSE, dependencies=TRUE, upgrade=TRUE, type=BTYPE, Ncpus=NCPUS)
 })})
