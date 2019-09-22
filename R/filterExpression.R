8af91f2a0b7e2d94f4145b8345cf01d19e741193
Christian Mertes <mertes@in.tum.de>
2019-09-22 16:44:13 +0200

    fix vapply with vectorization + fix indentation

--- R/filterExpression.R
+++ R/filterExpression.R
@@ -18,7 +18,8 @@
 #'             against the \code{fpkmCutoff}
 #' @param savefpkm If TRUE, the FPKM values are saved as assay
 #' @param addExpressedGenes If TRUE, adds 5 columns to the \code{colData} with
-#'            information regarding the number of expressed genes per sample
+#'             information regarding the number of expressed genes per sample.
+#'             By default FALSE.
 #' @param ... Additional arguments passed to \code{computeGeneLength}
 #' @return An OutriderDataSet containing the \code{passedFilter} column, which
 #'             indicates if the given gene passed the filtering threshold. If
@@ -42,16 +43,16 @@ setGeneric("filterExpression",
            function(x, ...) standardGeneric("filterExpression"))
 
 filterExpression.OUTRIDER <- function(x, gtfFile, fpkmCutoff=1, 
-                            percentile=0.95, filterGenes=TRUE, savefpkm=FALSE, 
-                            minCounts=FALSE, addExpressedGenes=FALSE, ...){
-  x <- filterMinCounts(x, filterGenes=filterGenes)
-  if(minCounts == TRUE){
-    return(x)
-  }
-  if(!missing(gtfFile)){
-    x <- computeGeneLength(x, gtfFile=gtfFile, ...)
-  }
-  filterExp(x, fpkmCutoff=fpkmCutoff, percentile=percentile,
+                    percentile=0.95, filterGenes=TRUE, savefpkm=FALSE, 
+                    minCounts=FALSE, addExpressedGenes=FALSE, ...){
+    x <- filterMinCounts(x, filterGenes=filterGenes)
+    if(minCounts == TRUE){
+        return(x)
+    }
+    if(!missing(gtfFile)){
+        x <- computeGeneLength(x, gtfFile=gtfFile, ...)
+    }
+    filterExp(x, fpkmCutoff=fpkmCutoff, percentile=percentile,
             filterGenes=filterGenes, savefpkm=savefpkm, 
             addExpressedGenes=addExpressedGenes)
 }
@@ -61,31 +62,30 @@ filterExpression.OUTRIDER <- function(x, gtfFile, fpkmCutoff=1,
 setMethod("filterExpression", "OutriderDataSet", filterExpression.OUTRIDER)
 
 filterExp <- function(ods, fpkmCutoff=1, percentile=0.95,
-                      filterGenes=filterGenes, savefpkm=savefpkm, 
-                      addExpressedGenes=FALSE){
-  fpkm <- fpkm(ods)
-  if(savefpkm){
-    assays(ods)[['fpkm']]<-fpkm
-  }
-  passed <- rowQuantiles(fpkm, probs=percentile) > fpkmCutoff
-  
-  mcols(ods)['passedFilter'] <- passed
-  
-  if(addExpressedGenes == TRUE){
-    dt <- computeExpressedGenes(fpkm, cutoff=fpkmCutoff, 
-                                percentile=percentile)
-    colData(ods) <- merge(colData(ods), dt, sort = FALSE)
-  }
-  validObject(ods)
-  
-  if(filterGenes==TRUE){
-    ods <- ods[passed == TRUE]
-  }
-  message(paste0(sum(!passed), ifelse(filterGenes, 
-          " genes are filtered out. ", " genes did not passed the filter. "),
-                 "This is ", signif(sum(!passed)/length(passed)*100, 3), 
-                 "% of the genes."))
-  return(ods)
+                    filterGenes=filterGenes, savefpkm=savefpkm, 
+                    addExpressedGenes=FALSE){
+    fpkm <- fpkm(ods)
+    if(savefpkm){
+        assays(ods)[['fpkm']] <- fpkm
+    }
+    passed <- rowQuantiles(fpkm, probs=percentile) > fpkmCutoff
+    mcols(ods)['passedFilter'] <- passed
+    
+    if(addExpressedGenes == TRUE){
+        dt <- computeExpressedGenes(fpkm, cutoff=fpkmCutoff, 
+                percentile=percentile)
+        colData(ods) <- merge(colData(ods), dt, sort=FALSE)
+    }
+    validObject(ods)
+    
+    if(filterGenes==TRUE){
+        ods <- ods[passed == TRUE]
+    }
+    message(paste0(sum(!passed), ifelse(filterGenes,
+            " genes are filtered out. ", " genes did not passed the filter. "),
+            "This is ", signif(sum(!passed)/length(passed)*100, 3), 
+            "% of the genes."))
+    return(ods)
 }
 
 #' 
@@ -119,48 +119,47 @@ filterExp <- function(ods, fpkmCutoff=1, percentile=0.95,
 #' 
 #' @export
 computeGeneLength <- function(ods, gtfFile, format='gtf', mapping=NULL, ...){
-  checkOutriderDataSet(ods)
-  
-  if(!is(gtfFile, "TxDb")){
-    #created txdb object
-    txdb <- makeTxDbFromGFF(gtfFile, format=format, ...)
-  } else {
-    txdb <- gtfFile
-  }
-  
-  # collect the exons per gene id
-  exons_list_per_gene <- exonsBy(txdb,by="gene")
-  
-  # for each gene, reduce all the exons to a set of non overlapping exons,
-  # calculate their lengths (widths) and sum then
-  widths <- width(reduce(exons_list_per_gene))
-  totalexonlength <- vapply(widths, sum, numeric(1))
-  if(!is.null(mapping)){
-    if(is.data.table(mapping)){
-      mapping <- as.data.frame(mapping)
+    checkOutriderDataSet(ods)
+    
+    if(!is(gtfFile, "TxDb")){
+        #created txdb object
+        txdb <- makeTxDbFromGFF(gtfFile, format=format, ...)
+    } else {
+        txdb <- gtfFile
     }
-    if(is.data.frame(mapping)){
-      tmpmapping <- mapping[,2]
-      names(tmpmapping) <- mapping[,1]
-      mapping <- tmpmapping
+    
+    # collect the exons per gene id
+    exons_list_per_gene <- exonsBy(txdb,by="gene")
+    
+    # for each gene, reduce all the exons to a set of non overlapping exons,
+    # calculate their lengths (widths) and sum then
+    widths <- width(reduce(exons_list_per_gene))
+    totalexonlength <- vapply(widths, sum, numeric(1))
+    if(!is.null(mapping)){
+        if(is.data.table(mapping)){
+            mapping <- as.data.frame(mapping)
+        }
+        if(is.data.frame(mapping)){
+            tmpmapping <- mapping[,2]
+            names(tmpmapping) <- mapping[,1]
+            mapping <- tmpmapping
+        }
+        names(totalexonlength) <- mapping[names(totalexonlength)]
     }
-    names(totalexonlength) <- mapping[names(totalexonlength)]
-  }
-  mcols(ods)['basepairs'] <- totalexonlength[rownames(ods)]
-  
-  # checking for NAs in basepair annotation
-  if(any(is.na(mcols(ods)['basepairs']))){
-    missingNames <- rownames(ods)[is.na(mcols(ods)['basepairs'])]
-    warning(paste0("Some genes (n=", length(missingNames), 
-              ") are not found in the annotation. Setting 'basepairs' == 1. ",
-                   "The first 6 names are:\n", 
-                   paste(missingNames[seq_len(min(6, length(missingNames)))],
-                         collapse=", ")))
-    mcols(ods[is.na(mcols(ods)['basepairs'])[,1]])['basepairs'] <- 1
-  }
-  
-  validObject(ods)
-  return(ods)
+    mcols(ods)['basepairs'] <- totalexonlength[rownames(ods)]
+    
+    # checking for NAs in basepair annotation
+    if(any(is.na(mcols(ods)['basepairs']))){
+        missingNames <- rownames(ods)[is.na(mcols(ods)['basepairs'])]
+        warning(paste0("Some genes (n=", length(missingNames), 
+                ") are not found in the annotation. Setting 'basepairs' == 1. ",
+                "The first 6 names are:\n", paste(collapse=", ",
+                        missingNames[seq_len(min(6, length(missingNames)))])))
+        mcols(ods[is.na(mcols(ods)['basepairs'])[,1]])['basepairs'] <- 1
+    }
+    
+    validObject(ods)
+    return(ods)
 }
 
 
@@ -195,56 +194,56 @@ NULL
 #' 
 #' @noRd
 filterMinCounts <- function(x, filterGenes=FALSE){
-  passed <- !checkCountRequirements(x, test=TRUE)
-  mcols(x)['passedFilter'] <- passed
-  
-  message(paste0(sum(!passed), " genes did not passed the filter due to ", 
-          "zero counts. This is ", signif(sum(!passed)/length(passed)*100, 3),
-                 "% of the genes."))
-  
-  if(isTRUE(filterGenes)){
-    x <- x[passed,]
-  }
-  return(x)
+    passed <- !checkCountRequirements(x, test=TRUE)
+    mcols(x)['passedFilter'] <- passed
+    
+    message(paste0(sum(!passed), " genes did not passed the filter due to ", 
+            "zero counts. This is ", signif(sum(!passed)/length(passed)*100, 3),
+            "% of the genes."))
+    
+    if(isTRUE(filterGenes)){
+        x <- x[passed,]
+    }
+    return(x)
 }
 
 
 #'
-#' Returns a data.table with the N of expressed genes per sample
+#' Returns a data.table with summary statistics per sample on number of genes
 #' 
 #' @noRd
-computeExpressedGenes <- function(x, cutoff=1, percentile=0.95){
-  
-  # Get cells that passed cutoff
-  cutoffPassedMatrix <- x > cutoff
-  
-  # Remove rows where no genes passed the cutoff
-  cutoffPassedMatrix <- cutoffPassedMatrix[rowSums(cutoffPassedMatrix) > 0, ]
-  
-  # Make a data.table with the expressed genes
-  expGenesDt <- data.table(sampleID = colnames(cutoffPassedMatrix), 
-                           expressedGenes = colSums(cutoffPassedMatrix))
-  setorder(expGenesDt, expressedGenes)
-  
-  cutoffPassedMatrix <- cutoffPassedMatrix[, expGenesDt$sample]
-  
-  # Get the cummulative sum of expressed genes
-  cumSumMatrix <- t(apply(cutoffPassedMatrix, 1, cumsum))
-  
-  # Get the genes common in all samples
-  expGenesDt$unionExpressedGenes <- colSums(cumSumMatrix > 0)
-  
-  # Get the genes that appear in at least 1 sample
-  expGenesDt$intersectionExpressedGenes <- 
-    colSums(vapply(seq_len(ncol(cumSumMatrix)), 
-                   function(j) cumSumMatrix[,j] == j,
-                   1L))
-  
-  expGenesDt$passedFilterGenes <- 
-    colSums(t(t( cumSumMatrix) / seq_len(ncol(cumSumMatrix)) ) >= 1-percentile)
-  
-  # Rank for plotting
-  expGenesDt[, expressedGenesRank := .I]
-  
-  return(expGenesDt)
+computeExpressedGenes <- function(fpkm, cutoff=1, percentile=0.95){
+    
+    # Get cells that passed cutoff
+    cutoffPassedMatrix <- fpkm > cutoff
+    
+    # Remove rows where no genes passed the cutoff
+    cutoffPassedMatrix <- cutoffPassedMatrix[rowSums(cutoffPassedMatrix) > 0, ]
+    
+    # Make a data.table with the expressed genes
+    expGenesDt <- data.table(sampleID = colnames(cutoffPassedMatrix), 
+                             expressedGenes = colSums(cutoffPassedMatrix))
+    
+    # order by sample rank
+    setorder(expGenesDt, expressedGenes)
+    cutoffPassedMatrix <- cutoffPassedMatrix[, expGenesDt$sample]
+    
+    # Get the cummulative sum of expressed genes
+    cumSumMatrix <- rowCumsums(cutoffPassedMatrix + 0)
+    
+    # Get the genes that appear in at least 1 sample
+    expGenesDt$unionExpressedGenes <- colSums(cumSumMatrix > 0)
+    
+    # Get the genes common in all samples
+    expGenesDt$intersectionExpressedGenes <- rowSums(
+            t(cumSumMatrix) == seq_col(cumSumMatrix))
+    
+    # Get number of genes passing the filtering
+    expGenesDt$passedFilterGenes <- colSums(
+            t(t(cumSumMatrix)/seq_col(cumSumMatrix)) >= 1-percentile)
+    
+    # Rank for plotting
+    expGenesDt[, expressedGenesRank := .I]
+    
+    return(expGenesDt)
 }
