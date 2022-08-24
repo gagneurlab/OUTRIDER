667cd1c983544285a1ecebde6c834e37cd749855
Smith Nicholas <smith@in.tum.de>
2022-08-24 16:02:47 +0200

    explicit Biostrings install

--- .github/helperScripts/setupEnv.R
+++ .github/helperScripts/setupEnv.R
@@ -41,7 +41,7 @@ if("windows" == .Platform$OS.type){
     installIfReq(p=c("XML", "xml2", "RSQLite", "progress", "AnnotationDbi", "BiocCheck"))
     
     print_log("Install source packages only for windows ...")
-    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene"),
+    INSTALL(c("Biostrings","GenomeInfoDbData", "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene"),
             type="both", version=BIOC_VERSION)                                                                          
 
 } else {
